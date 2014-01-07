d341a3fc53951656f6193bff6a9991f6937931e4
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-01-07 20:00:59 +0100

    Fix failing qglue tests

--- glue/tests/test_qglue.py
+++ glue/tests/test_qglue.py
@@ -34,7 +34,7 @@ class TestQGlue(object):
         self.xy = {'x': x, 'y': y}
         self.dict_data = {'u': u, 'v': v}
         self.recarray_data = np.rec.array([(0, 1), (2, 3)],
-                                          dtype=[('a', 'i'), ('b', 'i')])
+                                          dtype=[(b'a', int), (b'b', int)])
         self.astropy_table = Table({'x': x, 'y': y})
         self.bad_data = {'x': x, 'u': u}
 
