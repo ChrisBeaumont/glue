53089170cc57727c572a3445d95373b1e0dcf3c2
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-10-07 09:47:57 +0200

    Fix tests

--- glue/qt/widgets/tests/test_pv.py
+++ glue/qt/widgets/tests/test_pv.py
@@ -21,7 +21,7 @@ class TestSliceExtraction(object):
         slc = (0, 'y', 'x')
         x = [-0.5, 3.5]
         y = [0, 0]
-        s, _, _ = _slice_from_path(x, y, self.d, 'x', slc)
+        s, _ = _slice_from_path(x, y, self.d, 'x', slc)
         assert_allclose(s, self.x[:, 0, :])
 
     def test_constant_x(self):
@@ -29,14 +29,14 @@ class TestSliceExtraction(object):
         slc = (0, 'y', 'x')
         y = [-0.5, 2.5]
         x = [0, 0]
-        s, _, _ = _slice_from_path(x, y, self.d, 'x', slc)
+        s, _ = _slice_from_path(x, y, self.d, 'x', slc)
         assert_allclose(s, self.x[:, :, 0])
 
     def test_transpose(self):
         slc = (0, 'x', 'y')
         y = [-0.5, 3.5]
         x = [0, 0]
-        s, _, _ = _slice_from_path(x, y, self.d, 'x', slc)
+        s, _ = _slice_from_path(x, y, self.d, 'x', slc)
         assert_allclose(s, self.x[:, 0, :])
 
 
@@ -75,6 +75,7 @@ class MockImageWidget(object):
     def __init__(self, slice, data):
         self.slice = slice
         self.data = data
+        self.wcs = None
         self.client = MagicMock()
 
 
@@ -85,8 +86,7 @@ class TestPVSliceWidget(object):
         self.d = Data(x=np.zeros((2, 3, 4)))
         self.slc = (0, 'y', 'x')
         self.image = MockImageWidget(self.slc, self.d)
-        self.w = PVSliceWidget(np.zeros((3, 4)),
-                               [0, 1, 2, 3], [0, 1, 2], self.image)
+        self.w = PVSliceWidget(image=np.zeros((3, 4)), wcs=None, image_widget=self.image)
 
     def test_basic(self):
         pass
